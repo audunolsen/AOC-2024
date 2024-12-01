cff464eac35aed54f04e92b071a9e7268e885bae
Audun Meek Olsen <audunmeekolsen@gmail.com>
2024-12-01 22:07:25 +0100

    🎉

--- 1-historian-hysteria/main.js
+++ 1-historian-hysteria/main.js
@@ -1,52 +1,38 @@
 // node --watch ./1-historian-hysteria/main.js
 
-import { readFile } from 'node:fs/promises';
-const data = await readFile("./1-historian-hysteria/puzzle-input.txt", 'utf-8');
+import { readFile } from "node:fs/promises"
+
+const data = await readFile("./1-historian-hysteria/puzzle-input.txt", "utf-8")
+const lines = data.split("\n")
 
 let leftNumbers = []
 let rightNumbers = []
 
-for (const line of data.split('\n')) {
-  const numbers = line.split("   "); // e.g. "15112   39680" -> ["15112", "39680"]
-  const leftNumber = parseInt(numbers[0])
-  const rightNumber = parseInt(numbers[1])
-
-  leftNumbers.push(leftNumber) /** ["15112", "39680"][0] -> "15112" */  
-  rightNumbers.push(rightNumber) /** ["15112", "39680"][1] -> "39680" */
+for (const line of lines) {
+  const [leftNumber, rightNumber] = line.split(/\s+/).map(Number)
+  leftNumbers.push(leftNumber)
+  rightNumbers.push(rightNumber)
 }
 
-leftNumbers = leftNumbers.sort()
-rightNumbers = rightNumbers.sort()
+leftNumbers.sort()
+rightNumbers.sort()
 
-let totalDifference = 0;
-let similarityScore = 0;
+let totalDifference = 0
+let similarityScore = 0
 
-for (let i = 0; i < leftNumbers.length; i++) {
+for (const [i] of lines.entries()) {
+  const leftNumber = leftNumbers[i]
+  const rightNumber = rightNumbers[i]
 
+  totalDifference += Math.abs(rightNumber - leftNumber)
+  similarityScore +=
+    rightNumbers.filter(e => leftNumber === e).length * leftNumber
+}
+
+console.log({
   // Part 1
-  const leftNumber = leftNumbers[i];
-  const rightNumber = rightNumbers[i];
-
-  console.log({ leftNumber, rightNumber })
-
-  if (leftNumber > rightNumber) {
-    totalDifference += leftNumber - rightNumber
-  } else {
-    totalDifference += rightNumber - leftNumber
-  }
+  totalDifference,
 
   // Part 2
-
-  let matchCount = 0
-
-  for (const rightNumber of rightNumbers) {
-
-    if (leftNumber === rightNumber) {
-      matchCount++
-    }
-  }
-
-  similarityScore += leftNumber * matchCount
-}
-
-console.log({totalDifference, similarityScore})
+  similarityScore,
+})
